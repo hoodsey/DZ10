c5f4f5e8ad466e657dcfe6cd3d8c8683d61f33f0
Nasya Kabakova <kabakova.nas@gmail.com>
2023-03-11 23:12:03 +0300

    Add nextVolue

--- src/main/java/Radio.java
+++ src/main/java/Radio.java
@@ -1,2 +1,18 @@
 public class Radio {
+    public int currentNumber;//Номер текущей радиостанции
+    public int currentVolume;//Громкость звука
+
+    public void increaseVolume() { //метод увеличения звука на 1
+        if (currentVolume < 10) {
+            currentVolume = currentVolume + 1;
+        }
+    }
+    public void nextVolume() {//метод перекллючения звука по кнопке next
+        if (currentVolume == 9) {
+            currentVolume = 0;
+        } else {
+            increaseVolume();
+        }
+    }
+
 }
--- src/test/java/RadioTest.java
+++ src/test/java/RadioTest.java
@@ -1,2 +1,28 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class RadioTest {
+    @Test
+    public void shouldSetNumber() {
+        Radio radio = new Radio();
+        radio.currentNumber = 2;
+
+        int expected = 2;
+
+        long actual = radio.currentNumber;
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void shouldSetNextVolue() {
+        Radio radio = new Radio();
+        radio.currentVolume = 9;
+        radio.nextVolume();
+        int expected = 0;
+
+        long actual = radio.currentNumber;
+
+       assertEquals(expected, actual);
+    }
 }
